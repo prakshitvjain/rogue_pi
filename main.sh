2d9338d5fb0fc952c761606173ae71cd7607038b
prakshitvjain <prakshit17036@gmail.com>
2025-10-31 23:48:08 +0530

    added service scan and minor fix

--- main.sh
+++ main.sh
@@ -5,10 +5,15 @@
 set -e
 
 # find the subnet
-subnet = $(ip route | grep eth | grep -v default | awk '{print $1}')
+subnet=$(ip route | grep eth | grep -v default | awk '{print $1}')
 
-#perform host scan using nmap
+# perform host scan using nmap
 nmap -sn "$subnet" -T3 -oA host_scan
-cat host_scan | grep "scan report" | grep -oE '([0-9]{1,3}\.){3}[0-9]{1,3}' > hosts.txt
+grep "scan report" host_scan.gnmap | grep -oE '([0-9]{1,3}\.){3}[0-9]{1,3}' > hosts.txt
 
 # use for loop to perform individual service scan on all hosts 
+for ip in $(cat hosts.txt); do
+  i=1
+  sudo nmap "$ip" -Pn -sS -sV -v -sC -p- -T3 --max-retries 3 -D RND:10 -f -g 53 -oA "service_scan${i}"
+  ((i++))
+done
