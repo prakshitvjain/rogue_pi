4d5de2aecafe383f55856fbe89241d6afd2782dc
prakshitvjain <prakshit17036@gmail.com>
2025-11-13 23:24:50 +0530

    Fixed a dumb critical bug and grouping IPs as HOSTS and specific PORTS

--- main.sh
+++ main.sh
@@ -5,15 +5,30 @@
 set -e
 
 # find the subnet
-subnet=$(ip route | grep eth | grep -v default | awk '{print $1}')
+SUBNET=$(ip route | grep eth | grep -v default | awk '{print $1}')
 
 # perform host scan using nmap
-nmap -sn "$subnet" -T3 -oA host_scan
+nmap -sn "$SUBNET" -T3 -oA host_scan
 grep "scan report" host_scan.gnmap | grep -oE '([0-9]{1,3}\.){3}[0-9]{1,3}' > hosts.txt
 
 # use for loop to perform individual service scan on all hosts 
-for ip in $(cat hosts.txt); do
-  i=1
-  sudo nmap "$ip" -Pn -sS -sV -v -sC -p- -T3 --max-retries 3 -D RND:10 -f -g 53 -oA "service_scan${i}"
+i=1
+for IP in $(cat hosts.txt); do
+  sudo nmap "$IP" -Pn -sS -sV -v -sC -p- -T3 --max-retries 3 -D RND:10 -f -g 53 -oA "service_scan${i}"
   ((i++))
+done
+
+# group IPs as type of systems
+grep -E 'IIS|microsoft-ds|netbios-ssn|msrpc|epmap|Kerberos-Sec|MSWin|Windows|Exchange|SharePoint|MSFT' service_scan*.gnmap | awk '{print $2}' | sort -u > WIN_HOSTS.txt
+grep -E 'sshd|Apache|nginx|ProFTPD|vsFTPd|Postfix|Exim|BIND|lighttpd|Ubuntu|Debian|CentOS' service_scan*.gnmap | awk '{print $2}' | sort -u > LIN_HOSTS.txt
+
+# for loop to group using ports
+PORTS="21 22 23 25 53 80 88 110 135 139 143 443 445 1433 3306 3389 5000 5432 5985 5986 6379 8080 8500 9200 9300 11211 27017 47001"
+
+for OS in LIN WIN; do
+  INPUT_FILE="${OS}_HOSTS.txt"
+
+  for PORT in $PORTS; do
+    grep -f "$INPUT_FILE" service_scan*.gnmap | grep -E "${PORT}/open/tcp" | awk '{print $2}' | sort -u > ${OS}_PORT${PORT}.txt
+  done
 done
